1eed3351b0d25de3fce90d7634d42cb8ffece043
Bilal Ahmad <82260050+flutter99@users.noreply.github.com>
2024-02-07 13:29:30 +0500

    single post without model added

--- bloc_practice/lib/screens/single_post_page.dart
+++ bloc_practice/lib/screens/single_post_page.dart
@@ -10,17 +10,40 @@ class SinglePostPage extends StatefulWidget {
 }
 
 class _SinglePostPageState extends State<SinglePostPage> {
-  APIService _apiService = APIService();
+  APIService apiService = APIService();
   SinglePostModel singlePostModel = SinglePostModel();
+  dynamic singlePost;
+  bool isLoading = true;
 
-  getPost() {
-    _apiService.getSinglePost().then((value) {
+  /// get post with model
+  getPostWithModel() {
+    apiService.getSinglePostWithModel().then((value) {
       setState(() {
         singlePostModel = value!;
+        isLoading = false;
         print(singlePostModel);
       });
     }).onError((error, stackTrace) {
       print(error);
+      isLoading = false;
+      setState(() {});
+    });
+  }
+
+  /// get post without model
+
+  getPostWithoutModel() {
+    apiService.getSinglePostWithOutModel().then((value) {
+      setState(() {
+        singlePost = value;
+        isLoading = false;
+        print("Single Post $singlePost");
+      });
+    }).onError((error, stackTrace) {
+      print(error);
+      setState(() {
+        isLoading = false;
+      });
     });
   }
 
@@ -28,7 +51,8 @@ class _SinglePostPageState extends State<SinglePostPage> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    getPost();
+    //getPostWithModel();
+    getPostWithoutModel();
   }
 
   @override
@@ -39,53 +63,59 @@ class _SinglePostPageState extends State<SinglePostPage> {
     String body = singlePostModel.body.toString();
     return Scaffold(
       /// body
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const SizedBox(height: 30),
-              Text(
-                'User ID is: $userId',
-                style: const TextStyle(
-                  fontSize: 24,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w600,
+      body: isLoading == true
+          ? const Center(
+              child: CircularProgressIndicator(
+                color: Colors.black,
+              ),
+            )
+          : SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 30),
+                    Text(
+                      'User ID is: ${singlePost['userId'].toString()}',
+                      style: const TextStyle(
+                        fontSize: 24,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Text(
+                      'Post ID is: ${singlePost['id'].toString()}',
+                      style: const TextStyle(
+                        fontSize: 24,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Text(
+                      'Title is: ${singlePost['title'].toString()}',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Text(
+                      'Body is: ${singlePost['body'].toString()}',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 16),
-              Text(
-                'Post ID is: $id',
-                style: const TextStyle(
-                  fontSize: 24,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              const SizedBox(height: 16),
-              Text(
-                'Title is: $title',
-                style: const TextStyle(
-                  fontSize: 20,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-              const SizedBox(height: 16),
-              Text(
-                'Body is: $body',
-                style: const TextStyle(
-                  fontSize: 20,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
--- bloc_practice/lib/services/api_service.dart
+++ bloc_practice/lib/services/api_service.dart
@@ -4,7 +4,8 @@ import '../models/single_post_model.dart';
 import 'package:http/http.dart' as http;
 
 class APIService {
-  Future<SinglePostModel?> getSinglePost() async {
+  /// get single post with model
+  Future<SinglePostModel?> getSinglePostWithModel() async {
     try {
       Uri url = Uri.parse('https://jsonplaceholder.typicode.com/posts/1');
       http.Response response = await http.get(url);
@@ -12,6 +13,23 @@ class APIService {
         SinglePostModel model =
             SinglePostModel.fromJson(json.decode(response.body));
         return model;
+      }
+    } catch (e) {
+      print(e);
+    }
+
+    return null;
+  }
+
+  /// get single post without model
+  Future<dynamic> getSinglePostWithOutModel() async {
+    try {
+      Uri url = Uri.parse('https://jsonplaceholder.typicode.com/posts/1');
+      http.Response response = await http.get(url);
+      if (response.statusCode == 200) {
+        String body = response.body;
+        dynamic data = jsonDecode(body);
+        return data;
       }
     } catch (e) {
       print(e);
